b036edbd3a16468899defa0c2af4ba52ca509637
Carlos Hernan <86528810+Hernan-DOS@users.noreply.github.com>
2022-11-07 15:19:40 -0300

    Comienzo de proyecto
    Menu restaurant en java

---
+++ src/menu_restaurant_team_/noche/Menu_Restaurant_Team_Noche.java
@@ -0,0 +1,23 @@
+
+package menu_restaurant_team_.noche;
+
+
+public class Menu_Restaurant_Team_Noche {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Este es el menu restaurant");
+        
+    /*Menu restaurant*/ 
+    
+    /* Definimos las variables*/
+    String nom, bebidas, comidas;
+    int opcionMenu, acumulac, acumulab, seleccion, mesa;   
+    double sumar;
+        
+    }
+    
+    
+}
